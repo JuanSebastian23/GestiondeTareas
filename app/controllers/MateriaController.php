abe44a447baa71e37169373b560ccdbed990e8f0
Brandon Bernal <brandons-bernalr@unilibre.edu.co>
2025-02-24 23:02:53 -0500

    Guardando cambios en MateriaController antes de pull

--- app/controllers/MateriaController.php
+++ app/controllers/MateriaController.php
@@ -1,12 +1,15 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'] . '/GestiondeTareas/app/models/MateriaModel.php');
 
-class MateriaController
-{
-    public function obtenerMateriasEstudiante($estudiante_id)
-    {
-        $materiaModel = new MateriaModel();
-        return $materiaModel->getMateriasEstudiante($estudiante_id);
+class MateriaController {
+    private $materiaModel;
+
+    public function __construct() {
+        $this->materiaModel = new MateriaModel();
+    }
+
+    public function obtenerMateriasEstudiante($estudiante_id) {
+        return $this->materiaModel->obtenerMateriasPorEstudiante($estudiante_id);
     }
 }
 ?>
